81eb7106df2c1b11ff9495f8d77a5b4898a0254b
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-07-01 09:33:53 -0500

    GED-35 Structure project

---
+++ src/main/java/co/edu/uptc/Gestor_de_rutas/GestorDeRutasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Gestor_de_rutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeRutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeRutasApplication.class, args);
+	}
+
+}
