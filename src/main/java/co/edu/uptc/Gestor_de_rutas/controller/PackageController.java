310635e4d9b04cd1f0180f32a106e2766269a1ad
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-07-04 22:09:50 -0500

    GED-28 function change state package

--- src/main/java/co/edu/uptc/Gestor_de_rutas/controller/PackageController.java
+++ src/main/java/co/edu/uptc/Gestor_de_rutas/controller/PackageController.java
@@ -1,5 +1,36 @@
 package co.edu.uptc.Gestor_de_rutas.controller;
 
+import co.edu.uptc.Gestor_de_rutas.model.*;
+import co.edu.uptc.Gestor_de_rutas.model.Package;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
 public class PackageController {
+    private Package pack;
+    @Getter
+    private OrdenDelivery ordenDelivery;
+    public void createOrderDelivery(int id, Responsible responsible, Shopper shopper, LocalDate deadLine, State state,
+                                    String description, String observation, Package pack){
+        this.ordenDelivery = new OrdenDelivery(id,responsible,shopper,deadLine,state,description,observation,pack);
+    }
+
+    public boolean changeState(String state, OrdenDelivery ordenDelivery){
+        if (state.equals("Entregado")){
+            ordenDelivery.setState(State.DELIVERED);
+            return true;
+        }else if (state.equals("En camino")){
+            ordenDelivery.setState(State.ONTHEWAY);
+            return true;
+        }else if(state.equals("Devuelto")){
+            ordenDelivery.setState(State.RETURNED);
+            return true;
+        }else if(state.equals("Saliendo de bodega")){
+            ordenDelivery.setState(State.LEAVINGTHEWAREHOUSE);
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
