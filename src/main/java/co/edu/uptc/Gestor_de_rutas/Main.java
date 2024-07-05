fc5bd8a027c801b76f35d28ffbf9e9f6bc0ef44d
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-07-05 00:26:05 -0500

    GED-19 Dijkstra Algorithm test

--- src/main/java/co/edu/uptc/Gestor_de_rutas/Main.java
+++ src/main/java/co/edu/uptc/Gestor_de_rutas/Main.java
@@ -1,14 +1,20 @@
 package co.edu.uptc.Gestor_de_rutas;
 
+import co.edu.uptc.Gestor_de_rutas.controller.DijkstraAlgorithm;
 import co.edu.uptc.Gestor_de_rutas.controller.GraphController;
 
 public class Main {
     public static void main(String[] args) {
 
        GraphController graphController = new GraphController();
-
        graphController.createGraph();
-        System.out.println(graphController.getGraph().toString());
+        GraphController controller = new GraphController();
+        controller.createGraph();
+        DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm();
+        Long startNodeId = 1016196839L;
+        Long endNodeId = 7787924883L;
+        double distance = dijkstraAlgorithm.dijkstra(startNodeId, endNodeId, controller.getGraph());
+        System.out.println("Distancia desde " + startNodeId + " a " + endNodeId + " es " + distance);
 
     }
 
