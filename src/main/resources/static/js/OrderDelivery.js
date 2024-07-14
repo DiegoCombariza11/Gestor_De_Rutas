43af1922b7c25a45e97b95e8351f7db4f758abbd
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-07-14 13:46:40 -0500

    GED-23 implement api restfull and mongodb

--- src/main/resources/static/js/OrderDelivery.js
+++ src/main/resources/static/js/OrderDelivery.js
@@ -27,21 +27,3 @@ fetch("/orderDelivery/save",{
     .then(data => {
         console.log(data);
 })
-$(document).ready(function() {
-    $('.shipment-checkbox').on('change', function() {
-        var row = $(this).closest('tr');
-        var shipmentId = row.find('td:eq(1)').text();
-        var shipmentAddress = row.find('td:eq(2)').text();
-
-        if ($(this).is(':checked')) {
-            $('#send-box').append('<p id="shipment-' + shipmentId + '">ID: '+ shipmentId + ' - Dirección: ' + shipmentAddress + '</p>');
-        } else {
-            $('#shipment-' + shipmentId).remove();
-        }
-    });
-
-    $('#start-route-btn').on('click', function() {
-        // Code to start the route goes here
-        alert('Route started!');
-    });
-});
