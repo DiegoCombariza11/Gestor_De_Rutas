6157c247b72e992dffae4e7c4ae065cc4d0ac41b
Edwin Martinez <edwin.martinez04@uptc.edu.co>
2024-07-15 00:52:09 -0500

    GED-40 automatized Dijkstra Algorithm for more one point

--- src/main/java/co/edu/uptc/Gestor_de_rutas/Main.java
+++ src/main/java/co/edu/uptc/Gestor_de_rutas/Main.java
@@ -3,8 +3,11 @@ package co.edu.uptc.Gestor_de_rutas;
 import co.edu.uptc.Gestor_de_rutas.controller.DijkstraAlgorithm;
 import co.edu.uptc.Gestor_de_rutas.controller.GraphController;
 import co.edu.uptc.Gestor_de_rutas.controller.PathToGeoJson;
+import co.edu.uptc.Gestor_de_rutas.controller.RouteController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,13 +15,21 @@ public class Main {
         controller.createGraph();
         DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm();
         PathToGeoJson geoJsonMapper = new PathToGeoJson();
+        RouteController routeController = new RouteController();
         Long startNodeId = 1016196839L;
         Long endNodeId = 7787924883L;
-        System.out.println("Nodo de inicio: " + controller.getNodeById(startNodeId));
-        System.out.println("Nodo de fin: " + controller.getNodeById(endNodeId));
-        //double distance = dijkstraAlgorithm.dijkstra(startNodeId, endNodeId, controller.getGraph());
-        geoJsonMapper.convertPathToGeoJson(dijkstraAlgorithm.getShortestPath(startNodeId, endNodeId, controller.getGraph()).getVertexList(), controller, "src/main/resources/templates/path.geojson");
-        System.out.println("Camino más corto: " + dijkstraAlgorithm.getShortestPath(startNodeId, endNodeId, controller.getGraph()).getVertexList());
-        System.out.println("Distancia total del camino más corto: " + dijkstraAlgorithm.longPath(dijkstraAlgorithm.getShortestPath(startNodeId, endNodeId, controller.getGraph()).getVertexList(), controller));
+        List<Long> endNodes = List.of(1016183269L,startNodeId, endNodeId,1016185698L);
+//        List<Long> route= routeController.setRoute(endNodes, controller.getGraph(), controller);
+//        //double distance = dijkstraAlgorithm.dijkstra(startNodeId, endNodeId, controller.getGraph());
+//        System.out.println("Ruta: " + route);
+//        List<Long> path = new ArrayList<>();
+//        for (int i = 0; i < route.size() - 1; i++) {
+//            System.out.println("Ruta: " + route.get(i) + " -> " + route.get(i + 1));
+//            List<Long> aux = dijkstraAlgorithm.getShortestPath(route.get(i), route.get(i + 1), controller.getGraph()).getVertexList();
+//            System.out.println(aux+"\n");
+//            path.addAll(aux);
+//        }
+//        System.out.println("Ruta: " + path);
+        geoJsonMapper.convertPathToGeoJson(routeController.getPath(endNodes, controller.getGraph(), controller), controller, "src/main/resources/templates/path.geojson");
     }
 }
--- src/main/java/co/edu/uptc/Gestor_de_rutas/controller/RouteController.java
+++ src/main/java/co/edu/uptc/Gestor_de_rutas/controller/RouteController.java
@@ -1,6 +1,54 @@
 package co.edu.uptc.Gestor_de_rutas.controller;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RouteController {
     private DijkstraAlgorithm dijkstraAlgorithm;
 
+    public RouteController() {
+        this.dijkstraAlgorithm = new DijkstraAlgorithm();
+    }
+
+    public List<Long> setRoute(List<Long> endpoints, Graph<Long, DefaultEdge> graph, GraphController controller) {
+        List<Long> route = new ArrayList<>();
+        List<Long> remainingEndpoints = new ArrayList<>(endpoints);
+
+        route.add(remainingEndpoints.remove(0));
+
+        while (!remainingEndpoints.isEmpty()) {
+            Long currentEndpoint = route.get(route.size() - 1);
+            double shortestDistance = Double.MAX_VALUE;
+            Long nextEndpoint = null;
+
+            for (Long endpoint : remainingEndpoints) {
+                double distance = dijkstraAlgorithm.longPath(
+                        dijkstraAlgorithm.getShortestPath(currentEndpoint, endpoint, graph).getVertexList(),
+                        controller
+                );
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    nextEndpoint = endpoint;
+                }
+            }
+
+            if (nextEndpoint != null) {
+                route.add(nextEndpoint);
+                remainingEndpoints.remove(nextEndpoint);
+            }
+        }
+
+        return route;
+    }
+    public List<Long> getPath(List<Long> endPoints, Graph graph,GraphController controller){
+        List<Long> route= setRoute(endPoints, graph, controller);
+        List<Long> path = new ArrayList<>();
+        for (int i = 0; i < route.size() - 1; i++) {
+            List<Long> aux = dijkstraAlgorithm.getShortestPath(route.get(i), route.get(i + 1), graph).getVertexList();
+            path.addAll(aux);
+        }
+        return path;
+    }
 }
