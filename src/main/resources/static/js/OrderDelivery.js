383e4e80a0d79ecce2d7d9f293fccd3add084ae0
Diego Combariza <diego.combariza@uptc.edu.co>
2024-07-29 09:32:18 -0500

    Final fixed commit

--- src/main/resources/static/js/OrderDelivery.js
+++ src/main/resources/static/js/OrderDelivery.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
     $('#shipment-form').on('submit', function (event) {
         event.preventDefault();
-        var name = $('#buyer-name').val();
-        var lastName = $('#buyer-lastname').val();
-        var email = $('#buyer-email').val();
-        var contact = $('#buyer-contact').val();
-        var description = $('#product-description').val();
-        var observation = $('#product-observation').val();
-        var price = $('#product-price').val();
-        var weight = $('#product-weight').val();
-        var destination = $('#destination').val();
-        var nameProduct = $('#product-name').val();
-
-        if (!name || !lastName || !email || !contact || !description || !observation || !price || !weight || !destination || !nameProduct) {
-
+        function replaceWithEmptyString(value) {
+            return value.replace(/^\s+|\s+$/g, '') === '' ? '' : value;
+        }
+        var name = replaceWithEmptyString($('#buyer-name').val());
+        var lastName = replaceWithEmptyString($('#buyer-lastname').val());
+        var email = replaceWithEmptyString($('#buyer-email').val());
+        var contact = replaceWithEmptyString($('#buyer-contact').val());
+        var description = replaceWithEmptyString($('#product-description').val());
+        var observation = replaceWithEmptyString($('#product-observation').val());
+        var price = replaceWithEmptyString($('#product-price').val());
+        var weight = replaceWithEmptyString($('#product-weight').val());
+        var destination = replaceWithEmptyString($('#destination').val());
+        var nameProduct = replaceWithEmptyString($('#product-name').val());
+        if (!name || !lastName || !email || !contact || !description || !observation || price<0 || weight<0 || !destination || !nameProduct) {
+            document.getElementById('error-message').textContent = 'Por favor, rellene todos los campos correctamente.';
             return;
         }
-
         fetch("/orderDelivery/save", {
             method: 'POST',
             headers: {
@@ -40,7 +41,7 @@ $(document).ready(function () {
                 if (response.ok) {
                     window.location.href = '/pages/OrderDelivery.html';
                 } else {
-                    document.getElementById('error-message').textContent = 'Error con la direccion ingresada';
+                    document.getElementById('error-message').textContent = 'Error con la dirección ingresada.';
                 }
             })
             .catch(error => {
